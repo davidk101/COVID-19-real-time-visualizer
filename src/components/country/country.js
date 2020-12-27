a104cf6b16445665df7b7e91a0b599dd81fd7c17
David Kumar <david.dn.kumar@gmail.com>
2020-12-27 15:23:58 -0600

    add formControl and nativeSelect for country selector

--- src/components/country/country.js
+++ src/components/country/country.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {NativeSelect, FormControl} from "@material-ui/core"
+import styles from './country.module.css'
 
 const country = () => {
     return (
-        <h1>Country</h1>
+        <FormControl className = {styles.formControl}>
+            <NativeSelect>
+                <option value = "global"> Global</option>
+            </NativeSelect>
+        </FormControl>
     )
 }
 
