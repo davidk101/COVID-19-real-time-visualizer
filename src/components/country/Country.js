44c5012ec37c9da3ebc35a068f104c3cdd1ede8b
David Kumar <david.dn.kumar@gmail.com>
2020-12-31 13:54:23 -0600

    add links to footer

--- src/components/country/Country.js
+++ src/components/country/Country.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect } from 'react'
-import { NativeSelect, FormControl } from "@material-ui/core"
+import { NativeSelect, FormControl } from "@material-ui/core" // https://material-ui.com/components/selects/
 import styles from './Country.module.css'
 import { fetchCountries } from '../../api'
 
@@ -14,8 +14,8 @@ const Country = ({handleCountryChange}) => { // makes use of prop
         fetchAPI()
     }, [setFetchedCountries]) // second param is necessary - changes only when setFetchedCountries changes thereby enabling to pick different countries
     return (
-        <FormControl className = {styles.formControl}>
-            <NativeSelect defaultValue = "" onChange = {(e) => handleCountryChange(e.target.value)}> {/* callback function with an event and then calling function from props  */}
+        <FormControl className = {styles.formControl} variant = "standard">
+            <NativeSelect placeholder = "sel" defaultValue = "" onChange = {(e) => handleCountryChange(e.target.value)}> {/* callback function with an event and then calling function from props  */}
                 <option value = ""> Worldwide</option> {/* empty string will mean country = '' in Chart.js*/}
                 <option value = "United States"> United States</option>
                 {fetchedCountries.map((country, i) => <option key = {i} value = {country}>{country}</option>)} {/* adding 181 countries by mapping */}
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,11 +4,21 @@ import styles from './Footer.module.css'
 const Footer = () => {
     return(
         <div className = {styles.container}>
-            <a  href = " https://coronavirus.jhu.edu/map.html">
+            <a  href = "https://github.com/CSSEGISandData/COVID-19">
                 <div>
-                    <i className = "fa fa-info-circle"></i> Data extracted from John Hopkins Coronavirus Resource Center
+                    <i className = "fa fa-info-circle"></i>
+                    Data extracted from the John Hopkins University CSSE COVID-19 Data Repository
+                </div>
+                <div>
+                    <i className="fas fa-clock"></i>
+                    Updated daily between 04:45 and 05:15 GMT
                 </div>
             </a>
+
+            <div>
+                <i className="fas fa-user"></i>
+                Developed by David Kumar
+            </div>
         </div>
     )
 }
